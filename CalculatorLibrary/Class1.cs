20405d4eaa4b7b490ee2e873fd2c9c79cd219ed4
Rapeepan04 <Rapeepantongchai163@gmail.com>
2024-10-18 11:16:19 +0700

    first commit

---
+++ CalculatorLibrary/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace CalculatorLibrary;
+
+public static class CalcultorLibraby
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace HelloWorld;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a=1;
+        int b=2;
+        int c=0;
+        c=Add(a,b);
+        Console.WriteLine($"Result of Add ({a}{b}) is {c}");
+    }
+    static int Add(int var1,int var2)
+    {
+        return var1 + var2;
+    }
+}
